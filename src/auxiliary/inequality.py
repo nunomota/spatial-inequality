738b1d9f0ab9fb470eaa9c87f2c031e329a87972
Nuno Mota <nunomota@missinglink.technology>
2021-03-29 14:55:11 +0100

    Fixes LaTex notation and syntax errors

--- src/auxiliary/inequality.py
+++ src/auxiliary/inequality.py
@@ -5,12 +5,14 @@ def gini_index(benefit_vector):
     Calculate the gini index for a benefit distribution
     with N elements, according to the (latex) definition:
     
+    \\(
     \frac{
         \sum_{j=1}^{N} \sum_{i=1}^{N} \left| y_i - y_j \right|
     }{
         2 N \sum_{i=1}^{N} y_i
     }
-    
+    \\)
+
     Parameters:
     benefit_vector (list): List of float values, containing
         a benefit (or wealth) for multiple parties in a
@@ -26,26 +28,28 @@ def gini_index(benefit_vector):
     denominator = 2 * N * sum(benefit_vector)
     return numerator / denominator
     
-def spatial_index(benefit_vector, get_neighbours):
+def spatial_index(benefit_vector, get_neighbors):
     """
     Calculate spatial inequality for a benefit distribution
     with N elements, according to the (latex) definition:
     
+    \\(
     \frac{
         \sum_{i=1}^{N} \frac{1}{N_i} \sum_{j=1}^{N_i} \left| y_i - y_j \right|
     }{
         \sum_{i=1}^{N} y_i
     }
-    
+    \\)
+
     As opposed to the Gini Index, this definition only iterates
-    over elements that are considered 'neighbours' (and not
+    over elements that are considered 'neighbors' (and not
     every possible pair).
     
     Parameters:
     benefit_vector (list): List of float values, containing
         a benefit (or wealth) for multiple parties in a
         population
-    get_neighbours (func): Function that receives a
+    get_neighbors (func): Function that receives a
         single district's index as a parameter (from
         0 to N-1) and returns a list of indices of its
         neighbouring districts (each from 0 to N-1)
@@ -54,15 +58,15 @@ def spatial_index(benefit_vector, get_neighbours):
     float: Spatial inequality associated with a given benefit
         vector
     """
-    assert(isinstance(benefit_vector, list) and callable(get_neighbours))
+    assert(isinstance(benefit_vector, list) and callable(get_neighbors))
     N = len(benefit_vector)
     abs_diff = lambda pair: abs(benefit_vector[pair[0]] - benefit_vector[pair[1]])
     numerator = 0
     individual_ineqs = []
     for i in range(N):
-        neighbours = get_neighbours(i)
-        Ni = len(neighbours)
-        individual_ineq = sum([abs_diff((i, j)) for j in neighbours]) / Ni
+        neighbors = get_neighbors(i)
+        Ni = len(neighbors)
+        individual_ineq = sum([abs_diff((i, j)) for j in neighbors]) / Ni
         individual_ineqs.append(individual_ineq)
     overall_ineq = sum(individual_ineqs) / sum(benefit_vector)
     return overall_ineq, individual_ineqs
