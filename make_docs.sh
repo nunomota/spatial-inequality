ca62fd219ab8645c28477692ed41a5e8c9a8548e
nunomota <nunomota@users.noreply.github.com>
2021-04-06 13:11:11 +0100

    Updates doc-generation script

--- make_docs.sh
+++ make_docs.sh
@@ -1,6 +1,7 @@
 BUILD_DIR='../docs'
 
 cd spatial_inequality/ ;
+rm -r $BUILD_DIR/* ;
 pdoc3 --force --html . -o $BUILD_DIR -c latex_math=True ;
 mv $BUILD_DIR/spatial_inequality/* $BUILD_DIR ;
 rm -r $BUILD_DIR/spatial_inequality ;
