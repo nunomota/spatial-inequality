8aa1f674587d64ce521a31500c6f3c495ab83a43
nunomota <nunomota@users.noreply.github.com>
2021-04-06 13:03:31 +0100

    Moves documentation's root directory

--- make_docs.sh
+++ make_docs.sh
@@ -1,1 +1,7 @@
-cd spatial_inequality/ ; pdoc3 --force --html . -o ../docs -c latex_math=True ; cd ..
+BUILD_DIR='../docs'
+
+cd spatial_inequality/ ;
+pdoc3 --force --html . -o $BUILD_DIR -c latex_math=True ;
+mv $BUILD_DIR/spatial_inequality/* $BUILD_DIR ;
+rm -r $BUILD_DIR/spatial_inequality ;
+cd ..
