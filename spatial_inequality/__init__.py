9d270c9b4f6757ab83959e86b85e590ec5f33fed
nunomota <nunomota@users.noreply.github.com>
2021-04-06 17:20:18 +0100

    Updates docstring with map screenshot

--- spatial_inequality/__init__.py
+++ spatial_inequality/__init__.py
@@ -6,5 +6,7 @@
 
 Welcome to the documentation of the Spatial Inequality Index! This package was first published as part of our publication at The Web Conference 2021, [Fair Partitioning of Public Resources: Redrawing District Boundary to Minimize Spatial Inequality in School Funding](https://nunomota.github.io/assets/papers/www2021.pdf).
 
+![Map](https://github.com/nunomota/spatial-inequality/blob/master/assets/redistricting_before_and_after.png?raw=true)
+
 If you landed here by mistake and simply want to see our algorithm in action, you can instead visit our [interactive website](http://redistricting.mpi-sws.org). For anyone interested in contributing to this codebase, feel free to open a pull-request (or an issue) on our [GitHub](https://github.com/nunomota/spatial-inequality) repository.
 """
